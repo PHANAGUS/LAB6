8b9abc5425624061baa4c69cc7565cef377857bc
PHANAGUS <phanarin03072002@gmail.com>
2021-11-26 15:55:26 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,31 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main()
+{
+    int num;
+    int even=0, odd=0;
+    while(num != 0)
+    {
+        cout << "Enter an integer: ";
+        cin >> num;
+        if(num == 0)
+        {
+            cout << "#Even numbers = " << even << endl;
+            cout << "#Odd numbers = " << odd << endl;
+        }
+        else
+        {
+            if(num%2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+    }
+    
     return 0;
 }
