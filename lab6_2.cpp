ddf58b8a71a6a2054f780ff0f1d1db1040862dde
PHANAGUS <phanarin03072002@gmail.com>
2021-11-26 16:34:06 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
-
+#include<cmath>
 using namespace std;
+
+double deg2rad(double deg)
+{
+    double rad = deg*(M_PI/180);
+    return rad;
+}
+
+double rad2deg(double rad)
+{
+    double deg = rad*(180/M_PI);
+    return deg;
+}
+
+double findXComponent(double l1, double l2, double a1, double a2)
+{
+    double x = (l1*cos(a1)) + (l2*cos(a2));
+    return x;
+}
+
+double findYComponent(double l1, double l2, double a1, double a2)
+{
+    double y = (l1*sin(a1)) + (l2*sin(a2));
+    return y;
+}
+
+double pythagoras(double x,double y)
+{
+    return sqrt(pow(x,2)+pow(y,2));
+}
+
+void showResult(double l,double a)
+{
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << endl;
+    cout << "Length of the resultant vector = " << l << endl;
+    cout << "Direction of the resultant vector (deg) = " << a << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
